d35c3aa6788b49013f6695a6c6b9b0af453cc7e1
Mark Bennison <mark.bennison@live.co.uk>
2019-02-24 23:59:32 +0000

    Add project files.

---
+++ Asp4Bootstrap4/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Asp4Bootstrap4.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Asp4Bootstrap4/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Asp4Bootstrap4.Startup))]
+namespace Asp4Bootstrap4
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
